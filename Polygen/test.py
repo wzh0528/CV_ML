1e441eadbffa16087e46ca67e5605adbdfa02cb4
Kris Wang <33029136+wzh0528@users.noreply.github.com>
2021-10-19 10:25:54 +0800

    Delete test.py

--- Polygen/test.py
+++
@@ -1,8 +0,0 @@
-import six
-a='82386/243/7246'
-b='60947//7245'
-print(six.ensure_str(a).split('/'))
-for index in six.ensure_str(a).split('/'):
-    if not index:
-        continue
-    print(index)
